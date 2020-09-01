86bd2804e79a6a3d6734b7ddf09caeb23b5765ea
haleighdalke <haleighdalke@gmail.com>
2020-09-01 16:13:52 -0400

    added 2 model methods

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,9 @@ class Admin < ApplicationRecord
   has_many :festivals
   has_many :artists, through: :festivals
   has_many :pieces, through: :festivals
+
+  def getName
+    self.user.name
+  end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,9 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :pieces
   has_many :festivals, through: :pieces
+
+  def getName
+    self.user.name
+  end
+  
 end
